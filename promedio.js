14f55ec5aee3e99a036a9f3fede9081203f61399
Santiago Tambone <tambone.santiago@gmail.com>
2022-07-05 16:38:22 -0300

    Agregando function

--- promedio.js
+++ promedio.js
@@ -1,24 +1,40 @@
+let repetir = true
+const promedio = (a, b) => { return (a + b) / 2 }
+
+
 do {
-    let nota1 = parseFloat(prompt("Ingrese la nota de su primer parcial:"))
-    let nota2 = parseFloat(prompt("Ingrese la nota de su segundo parcial:"))
-    let promedio = (nota1 + nota2) /2
- 
-    if(isNaN(nota1) || isNaN(nota2)) {
-        alert("Por favor ingrese una nota válida")
-    }
-    if(promedio >= 8) {
-        alert(`¡Felicitaciones! Con un promedio de ${promedio} ha promocionado la materia`)
+    nota1 = parseFloat(prompt("Ingrese la nota de su primer parcial"))
+    if(nota1 <1 || nota1 >10 || isNaN(nota1)) {
+        alert("Por favor ingrese una nota válida entre 1 y 10")
+    } else {
         break
     }
-    if(promedio < 8 && promedio >= 4) {
-        alert(`Su promedio es de ${promedio}, va a final`)
+} while(repetir)
+
+do {
+    nota2 = parseFloat(prompt("Ingrese la nota de su segundo parcial"))
+    if(nota2 < 1 || nota2 > 10 || isNaN(nota2)) {
+        alert("Por favor ingrese una nota válida entre 1 y 10")
+    } else {
         break
     }
-    if(promedio < 4) {
-        alert(`Su promedio es de ${promedio}, debe recursar la materia`)
-    }
-    
-} while(isNaN(nota1) || isNaN(nota2))
+} while(repetir)
+
+let notaFinal = promedio(nota1, nota2)
+
+if(notaFinal < 4) {
+    alert(`Su promedio es de ${notaFinal}. Debe recursar la materia`)
+}
+
+if(notaFinal >= 4 && notaFinal < 8) {
+    alert(`Su promedio es de ${notaFinal}. Debe rendir examen final`)
+}
+
+if(notaFinal >= 8) {
+    alert(`Su promedio es de ${notaFinal} ¡Felicitacioes! Ha promocionado la materia`)
+}
+
+
 
 
 
